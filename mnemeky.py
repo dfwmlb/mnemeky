9f4d3e50a2b1e5fa215850f7b9e8f9699b312790
dmitrij.knjazev <dmitrij.knjazev@inplatlabs.ru>
2019-12-04 12:58:11 +0300

    making english descriptions

--- mnemeky.py
+++ mnemeky.py
@@ -31,38 +31,37 @@ def job():
         http = credentials.authorize(httplib2.Http())
         service = discovery.build('calendar', 'v3', http=http)
 
-        now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-        now_1day = round(time.time())+86400 #плюс сутки
+        now = datetime.datetime.utcnow().isoformat() + 'Z'
+        now_1day = round(time.time())+86400
         now_1day = datetime.datetime.fromtimestamp(now_1day).isoformat() + 'Z'
 
-        print('Берем 100 событий')
         eventsResult = service.events().list(
             calendarId=config.gcalendar_id, timeMin=now, timeMax=now_1day, maxResults=100, singleEvents=True,
             orderBy='startTime').execute()
         events = eventsResult.get('items', [])
 
         if not events:
-            print('Сегодня событий нет')
-            bot.send_message(config.tg_chat_id, 'Сегодня событий нет')
+            print('There are no events today')
+            bot.send_message(config.tg_chat_id, 'There are no events today')
         else:
-            msg = '<b>События сегодня:</b>\n'
+            msg = '<b>There is event today</b>\n'
             for event in events:
                 start = event['start'].get('dateTime', event['start'].get('date'))
                 if "summary" not in event:
-                    ev_title = 'Без названия'
+                    ev_title = 'No name'
                     print(ev_title)
                 else:
                     print(start,' ', event['summary'])
                     ev_title = event['summary']
                 if "description" not in event:
-                    ev_desc = 'Без описания'
+                    ev_desc = 'No description'
                     print(ev_desc)
                 else:
                     print(event['description'])
                     ev_desc = event['description']
 
                 
-                cal_link = '<a href="/%s">Подробнее...</a>'%event['htmlLink']
+                cal_link = '<a href="/%s">More...</a>'%event['htmlLink']
                 ev_start = event['start'].get('dateTime')
                 print (cal_link)
                 msg = msg+'%s\n%s\n%s\n%s\n\n'%(ev_title, ev_start, ev_desc, cal_link)
